2d06cb3b85f179ec4cf3c1ab642b67d763de3165
daniel302 <daniel302@revature.net>
2022-05-16 18:34:16 -0400

    Initial test

--- src/main/java/com/revature/developercorner/controller/BlogController.java
+++ src/main/java/com/revature/developercorner/controller/BlogController.java
@@ -1,4 +1,8 @@
 package com.revature.developercorner.controller;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BlogController {
 }
