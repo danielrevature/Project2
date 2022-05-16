ec0275171ec4bb0ecfe0bdebaf857a7299efde53
jhedler <jmhedler@gmail.com>
2022-05-16 15:09:58 -0500

    Fixed User to auto-generate User table

--- src/main/java/com/revature/developercorner/entity/User.java
+++ src/main/java/com/revature/developercorner/entity/User.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 //Lombok annotations
 @Data
@@ -14,9 +12,10 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @Entity
 
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long user_id;
     private String username;
     private String eMail;
