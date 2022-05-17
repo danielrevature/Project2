e767f4aaac74308761ffc6831a6c6c4999f31969
Chandresh <chandresh250@revature.net>
2022-05-17 14:14:02 -0500

    made changes

--- src/main/java/com/revature/developercorner/service/UserService.java
+++ src/main/java/com/revature/developercorner/service/UserService.java
@@ -1,4 +1,24 @@
 package com.revature.developercorner.service;
 
+
+import com.revature.developercorner.data.UserRepository;
+import com.revature.developercorner.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    //connection to the userrepository
+    @Autowired
+    UserRepository userRepository;
+
+    public void add_user(User user){
+        UserRepository.save(user);
+    }
+
+
+
+
+
 }
